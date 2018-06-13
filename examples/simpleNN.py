08e91d2c1189c583920eb3ac1cb78640b9ad4c48
prakharchoudhary <prakhar2397@gmail.com>
2018-06-13 17:40:47 +0530

    add an example of training on MNIST using the DL architecture

---
+++ examples/simpleNN.py
@@ -0,0 +1,20 @@
+"""
+This is a basic example used to train on MNIST dataset. The neural net
+involves:
+- 3 Dense Layers
+- ReLU activation for hidden layers
+- Sigmoid activation for output layer
+
+TODO:
+-----
+- [] Make a utils file that can load mnist data into three different
+sets: training, validation and testing.
+
+- [x] Prepare the sequential network class to stack layers and allow training
+and prediction.
+
+- [] Remember to checkpoint the training as well as save model and weights at the end.
+
+- [] Add tqdm based progress bar to show training and prediction progress.
+
+"""
